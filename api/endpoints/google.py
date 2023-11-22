1f6868954aedb2db7c972af535d6cc8422641940
Stefan <bogdan.izdrail@gmail.com>
2023-11-22 19:49:40 +0000

    Fixed the google news search by keyword

--- api/endpoints/google.py
+++ api/endpoints/google.py
@@ -1,6 +1,14 @@
 import json
+import base64
 import newspaper
 import datetime as dt
+
+"""Decode encoded Google News entry URLs."""
+import base64
+import functools
+import re
+
+
 from datetime import datetime
 
 from fastapi import APIRouter, Path, Query, Depends
@@ -14,11 +22,14 @@ pytrends = TrendReq(hl='en-GB', tz=360)
 
 router = APIRouter()
 
-
 now = dt.date.today()
 now = now.strftime('%m-%d-%Y')
 yesterday = dt.date.today() - dt.timedelta(days=1)
 yesterday = yesterday.strftime('%m-%d-%Y')
+
+
+
+
 
 
 class GoogleAction(BaseModel):
@@ -82,12 +93,30 @@ def remove_unwanted_part(url):
     return url
 
 
-def remove_unwanted_characters(link):
-    # Find the index of "&ved=" in the link
-    index = link.find("&ved=")
-    if index != -1:
-        return link[:index]
-    return link
+# Ref: https://stackoverflow.com/a/59023463/
+
+_ENCODED_URL_PREFIX = "https://news.google.com/rss/articles/"
+_ENCODED_URL_PREFIX_WITH_CONSENT = "https://consent.google.com/m?continue=https://news.google.com/rss/articles/"
+_ENCODED_URL_RE = re.compile(fr"^{re.escape(_ENCODED_URL_PREFIX_WITH_CONSENT)}(?P<encoded_url>[^?]+)")
+_ENCODED_URL_RE = re.compile(fr"^{re.escape(_ENCODED_URL_PREFIX)}(?P<encoded_url>[^?]+)")
+_DECODED_URL_RE = re.compile(rb'^\x08\x13".+?(?P<primary_url>http[^\xd2]+)\xd2\x01')
+
+
+@functools.lru_cache(2048)
+def _decode_google_news_url(url: str) -> str:
+    match = _ENCODED_URL_RE.match(url)
+    encoded_text = match.groupdict()["encoded_url"]  # type: ignore
+    encoded_text += "==="  # Fix incorrect padding. Ref: https://stackoverflow.com/a/49459036/
+    decoded_text = base64.urlsafe_b64decode(encoded_text)
+
+    match = _DECODED_URL_RE.match(decoded_text)
+    primary_url = match.groupdict()["primary_url"]  # type: ignore
+    primary_url = primary_url.decode()
+    return primary_url
+
+
+def decode_google_news_url(url: str) -> str:
+    return _decode_google_news_url(url) if url.startswith(_ENCODED_URL_PREFIX) else url
 
 
 @router.post("/google-news-search")
@@ -96,14 +125,19 @@ async def root(google: GoogleAction):
     cached_result = google_news_cache.get(keyword)
     if cached_result:
         return {"data": cached_result}
+
     google_news = GNews()
     google_news.period = '1d'
     google_news.max_results = 30
-    google_news.country = 'United Kingdom'
+    google_news.country = 'GB'
     results_dict = google_news.get_news(keyword)
+
+    # Apply the clean_url function to each entry
+    for entry in results_dict:
+        entry["url"] = decode_google_news_url(entry.get("url", ""))
+
     google_news_cache[keyword] = results_dict
     return {"data": results_dict}
-
 
 
 google_topic_cache = TTLCache(maxsize=1000, ttl=6 * 60 * 60)
